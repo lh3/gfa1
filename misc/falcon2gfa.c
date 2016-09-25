4d6bcae04b1df86a4beebd0339497541902305b1
Heng Li <lh3@me.com>
2016-09-25 00:20:36 -0400

    more correct falcon2gfa

--- misc/falcon2gfa.c
+++ misc/falcon2gfa.c
@@ -1,19 +1,56 @@
 #include <stdio.h>
 #include <ctype.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
-#include <unistd.h>
 #include <assert.h>
+#include <unistd.h>
 #include <zlib.h>
+#include "kvec.h"
 #include "kseq.h"
 KSTREAM_INIT(gzFile, gzread, 65536)
 
+#include "khash.h"
+KHASH_MAP_INIT_STR(str, int)
+
+typedef struct {
+	uint32_t v, w, x[3];
+} fcline_t;
+
+typedef struct {
+	char *name;
+	int len;
+} fcseg_t;
+
+typedef struct {
+	kvec_t(fcline_t) lines;
+	kvec_t(fcseg_t) segs;
+	khash_t(str) *h;
+} falcon_t;
+
+unsigned fc_add_seg(falcon_t *fc, const char *s)
+{
+	khint_t k;
+	int absent;
+	k = kh_put(str, fc->h, s, &absent);
+	if (absent) {
+		fcseg_t *t;
+		kv_pushp(fcseg_t, fc->segs, &t);
+		t->name = strdup(s);
+		t->len = 0;
+		kh_key(fc->h, k) = t->name;
+		kh_val(fc->h, k) = kh_size(fc->h) - 1;
+	}
+	return kh_val(fc->h, k);
+}
+
 int main(int argc, char *argv[])
 {
-	int c, dret;
+	int c, dret, i;
 	gzFile fp;
 	kstream_t *ks;
 	kstring_t str = {0,0,0};
+	falcon_t fc;
 
 	while ((c = getopt(argc, argv, "")) >= 0);
 
@@ -22,18 +59,20 @@ int main(int argc, char *argv[])
 		return 1;
 	}
 
-	fp = gzopen(argv[optind], "r");
+	fp = strcmp(argv[optind], "-")? gzopen(argv[optind], "r") : gzdopen(fileno(stdin), "r");
 	assert(fp);
 	ks = ks_init(fp);
+	memset(&fc, 0, sizeof(falcon_t));
+	fc.h = kh_init(str);
 
 	while (ks_getuntil(ks, KS_SEP_LINE, &str, &dret) >= 0) {
 		char *q = str.s, *p, *s[2], *type = 0;
 		int i, o[2], x[3], is_err = 0;
 		float identity = -1.;
+		fcline_t *t;
 		for (i = 0, p = q;; ++p) {
 			if (*p == 0 || isspace(*p)) {
 				c = *p, *p = 0;
-				//fprintf(stderr, "%s\n", q);
 				if (i == 0 || i == 1) {
 					if (p - q < 3 || (*(p-1) != 'B' && *(p-1) != 'E') || *(p-2) != ':') {
 						is_err = 1;
@@ -54,11 +93,30 @@ int main(int argc, char *argv[])
 		if (x[0] < x[1] && o[1]) is_err = 1;
 		if (x[0] > x[1] && !o[1]) is_err = 1;
 		if (is_err || type == 0) continue;
-		printf("L\t%s\t%c\t%s\t%c\t", s[0], "+-"[o[0]], s[1], "+-"[o[1]]);
-		if (x[0] < x[1]) printf(":%d\tL2:i:%d\n", x[0], x[1] - x[0]);
-		else printf(":%d\tL2:i:%d\n", x[2], x[0]);
+		kv_pushp(fcline_t, fc.lines, &t);
+		t->v = fc_add_seg(&fc, s[0]) << 1 | o[0];
+		t->w = fc_add_seg(&fc, s[1]) << 1 | o[1];
+		t->x[0] = x[0], t->x[1] = x[1], t->x[2] = x[2];
 	}
 
+	for (i = 0; i < fc.lines.n; ++i) {
+		fcline_t *t = &fc.lines.a[i];
+		int len = t->x[0] < t->x[1]? t->x[1] : t->x[0] + t->x[2];
+		if (fc.segs.a[t->w>>1].len < len)
+			fc.segs.a[t->w>>1].len = len;
+	}
+	for (i = 0; i < fc.segs.n; ++i) {
+		fcseg_t *t = &fc.segs.a[i];
+		printf("S\t%s\t*\tLN:i:%d\n", t->name, t->len);
+	}
+	for (i = 0; i < fc.lines.n; ++i) {
+		fcline_t *t = &fc.lines.a[i];
+		printf("L\t%s\t%c\t%s\t%c", fc.segs.a[t->v>>1].name, "+-"[t->v&1], fc.segs.a[t->w>>1].name, "+-"[t->w&1]);
+		if (t->x[0] < t->x[1]) printf("\t:%d\n", t->x[0]);
+		else printf("\t:%d\n", fc.segs.a[t->w>>1].len - t->x[0]);
+	}
+
+	kh_destroy(str, fc.h);
 	ks_destroy(ks);
 	gzclose(fp);
 	return 0;
